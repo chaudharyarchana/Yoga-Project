2d49a8b8e0a75593440b6ff16675738dc469a838
Archana Kumari <archanakumari123.co@gmail.com>
2024-01-07 21:23:18 +0530

    "updated"

--- src/Components/ToggleMenu.js
+++ src/Components/ToggleMenu.js
@@ -4,44 +4,46 @@ import Image from "next/image";
 import Link from "next/link";
 
 const ToggleMenu = () => {
-  const [isMenuOpen, setMenuOpen] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false)
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
   };
 
   return (
-    <div>
+    <div >
+      <div >
       <button
-        className="text-gray-800 hover:text-gray-600 transition duration-300"
+        className="text-gray-800 hover:text-gray-600 transition duration-300 "
         onClick={toggleMenu}
       >
         <Image src="/images/img00.png" alt="image" width={24} height={12} />
       </button>
-      <div>
+      </div>
+      <div className="absolute top-16 left-0">
         {isMenuOpen && (
-          <div className="lg:hidden ">
-            <div className="flex flex-col   space-y-2 mt-4">
+          <div className="lg:hidden w-96 bg-gray-800 py-1 text-white cursor-pointer" style={{width:"100vw"}}>
+            <div className="flex justify-evenly  align-middle">
               <Link
                 href="/"
-                className="text-gray-800 hover:text-gray-600 transition duration-300"
+                className=" hover:text-gray-600 transition duration-300"
               >
                 Home
               </Link>
               <Link
                 href="/classes"
-                className="text-gray-800 hover:text-gray-600 transition duration-300"
+                className=" hover:text-gray-600 transition duration-300"
               >
                 Classes
               </Link>
               <Link
                 href="/shop"
-                className="text-gray-800 hover:text-gray-600 transition duration-300"
+                className=" hover:text-gray-600 transition duration-300"
               >
                 Shop
               </Link>
               <Link
                 href="/about"
-                className="text-gray-800 hover:text-gray-600 transition duration-300"
+                className=" hover:text-gray-600 transition duration-300"
               >
                 About
               </Link>
